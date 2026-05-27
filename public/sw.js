24df66152e2d3594658bb10e5626b62a40d68924
froggymannnyea <jennifervu1950@icloud.com>
2026-05-26 20:39:29 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,23 @@
-const { ScramjetController } =
-    $scramjetLoadController();
+importScripts("/scram/scramjet.all.js");
+
+const { ScramjetServiceWorker } =
+    $scramjetLoadWorker();
 
 const scramjet =
-    new ScramjetController({
-        files: {
-            wasm: "/scram/scramjet.wasm.wasm",
-            all: "/scram/scramjet.all.js",
-            sync: "/scram/scramjet.sync.js",
-        }
-    });
+    new ScramjetServiceWorker();
 
-scramjet.init();
+self.addEventListener("fetch", event => {
+
+event.respondWith((async () => {
+
+await scramjet.loadConfig();
+
+if (scramjet.route(event)) {
+    return scramjet.fetch(event);
+}
+
+return fetch(event.request);
+
+})());
+
+});
